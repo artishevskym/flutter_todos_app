b2269225f8f7430aca61222a4f87efb4cecf3767
Martin Artishevsky <artishevsky.m@gmail.com>
2021-02-10 22:59:24 +0000

    Clean test code for TestEntity guard

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,8 @@ import 'package:flutter_todos_app/todo_entity.dart';
 
 /*
 TODO:
-- [DONE] Todos App
-- Todo entity
+- [DONE] TodosApp
+- [DONE] TodoEntity
 - Todo screen
 - Todo list
 - Add todo dialog
@@ -16,8 +16,6 @@ void main() => runApp(TodosApp());
 class TodosApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    TodoEntity entity = TodoEntity(title: '');
-
     return MaterialApp(
       title: 'Todos App',
       home: Scaffold(),
