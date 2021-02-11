160282a6f89f30b6253efe97eef49c6bcc193d28
Martin Artishevsky <artishevsky.m@gmail.com>
2021-02-11 00:15:46 +0000

    Clean comments

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todos_app/todo_entity.dart';
 import 'package:flutter_todos_app/todo_list_screen.dart';
 
-/*
-TODO:
-- [DONE] TodosApp
-- [DONE] TodoEntity
-- [DONE] Todo screen
-- [DONE] Todo list
-- [DONE] Add todo dialog
-- [DONE] Navigation to dialog
-- [DONE] Remove done logic
-- [DONE] Test scenarios
- */
 void main() => runApp(TodosApp());
 
 class TodosApp extends StatelessWidget {
